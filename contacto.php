2e6e7f13ad348fa25311e77799af724534d0aa08
nicolasguaragna <132408707+nicolasguaragna@users.noreply.github.com>
2023-09-23 13:02:06 -0300

    Agregado contacto

--- contacto.php
+++ contacto.php
@@ -13,40 +13,47 @@
     <script src="/script.js"></script>
 </head>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container">
-    <a class="navbar-brand" href="#"><img src="./img/stanley-logo.png" alt="logo" class="logo"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<div class="alert alert-dark custom-alert" role="alert">
+    ENVÍO GRATIS desde $25.000. LLEGA HOY 🚚 [CABA/GBA] comprando hasta las 12PM
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Productos
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Termos</a>
-            <a class="dropdown-item" href="#">Mates</a>
-            <a class="dropdown-item" href="#">Vasos</a>
-            <a class="dropdown-item" href="#">Mugs</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Ver todos los productos</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">FAQ</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Info Envíos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="contacto.php">Contacto</a>
-        </li>
-      </ul>
+</div>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#"><img src="./img/stanley-logo.png" alt="logo" class="logo"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Productos
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Termos</a>
+                <a class="dropdown-item" href="#">Mates</a>
+                <a class="dropdown-item" href="#">Vasos</a>
+                <a class="dropdown-item" href="#">Mugs</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Ver todos los productos</a>
+            </div>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">FAQ</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Info Envíos</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="contacto.php">Contacto</a>
+            </li>
+        </ul>
+        </div>
     </div>
-  </div>
-</nav>
+    </nav>
 <body>
     <h1>Ponete en contacto</h1>
 
