a1c67ca287c719f7c51762cfbaaa8cedcf65dea5
nicolasguaragna <132408707+nicolasguaragna@users.noreply.github.com>
2023-09-23 13:18:54 -0300

    ok formulario

--- datos_post.php
+++ datos_post.php
@@ -10,14 +10,14 @@ echo "</pre>";
 
 $nombre = $datos['nombre'];
 $apellido = $datos['apellido'];
-$email = $datos['email'];
+$email = $datos['correo'];
 $telefono = $datos['telefono'];
 $mensaje = $datos['mensaje'];
 
 
 echo "<p>El dato del campo nombre es: $nombre </p>";
 echo "<p>El dato del campo apellido es: $apellido </p>";
-echo "<p>El dato del campo email es: $email </p>";
+echo "<p>El dato del campo email es: $correo </p>";
 echo "<p>El dato del campo telefono es: $telefono </p>";
 echo "<p>El dato del campo mensaje es: $mensaje </p>";
 
