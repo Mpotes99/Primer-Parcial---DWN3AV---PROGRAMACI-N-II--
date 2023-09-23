5a79a31c85029098e420150ac30d697197edc53a
nicolasguaragna <132408707+nicolasguaragna@users.noreply.github.com>
2023-09-23 12:56:16 -0300

    eliminar datos post

--- contacto.php
+++ contacto.php
@@ -1,22 +1,37 @@
-<?PHP
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contacto</title>
+</head>
+<body>
+    <h1>Ponete en contacto</h1>
 
-echo "<h1>Datos via Post<h1>";
+    <form action="#" method="POST">
+        <div>
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre" id="nombre" required>
+        </div>
+        <div>
+            <label for="nombre">Apellido:</label>
+            <input type="text" name="nombre" id="nombre" required>
+        </div>
+        <div>
+            <label for="nombre">Email:</label>
+            <input type="text" name="nombre" id="nombre" required>
+        </div>
+        <div>
+            <label for="nombre">Telefono:</label>
+            <input type="text" name="nombre" id="nombre" required>
+        </div>
+        <div>
+            <label for="pass">Mensaje:</label>
+            <input type="password" name="pass" id="pass" required>
+        </div>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
 
-$datos = $_POST;
-
-echo "<pre>";
-print_r($datos);
-echo "</pre>";
-
-$nombre = $datos['nombre'];
-$apellido = $datos['apellido'];
-$email = $datos['email'];
-$telefono = $datos['telefono'];
-$mensaje = $datos['mensaje'];
-
-
-echo "<p>El dato del campo nombre es: $nombre </p>";
-echo "<p>El dato del campo apellido es: $apellido </p>";
-echo "<p>El dato del campo email es: $email </p>";
-echo "<p>El dato del campo telefono es: $telefono </p>";
-echo "<p>El dato del campo mensaje es: $mensaje </p>";
