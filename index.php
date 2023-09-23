e68e123cd8d9982dd528ecbe331f8ecc6ce8c873
Mauricio Potes <potesmauricio99@gmail.com>
2023-09-23 10:54:02 -0300

    cambie el nombre

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tu Tienda Stanley</title>
+    <title>Tu Tienda Stanley v2</title>
     <!-- Enlaza los archivos CSS de Bootstrap y tu propio CSS personalizado -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
