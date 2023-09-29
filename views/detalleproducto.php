29ef2ea829870900c9a579a2c488d0adc908751d
Mauricio Potes <potesmauricio99@gmail.com>
2023-09-29 18:02:58 -0300

    img

---
+++ views/detalleproducto.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <h1>Todos los Productos</h1>
+    
+    <div class="row">
+        <?php foreach ($productos as $producto) { ?>
+            <div class="col-12 col-md-4 mb-4">
+                <div class="card">
+                    <img src="<?= $producto->getImagen() ?>" class="card-img-top" alt="<?= $producto->getModelo() ?>">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $producto->getModelo() ?></h5>
+                        <p class="card-text">Tipo: <?= $producto->getTipo() ?></p>
+                        <p class="card-text">Capacidad: <?= $producto->getCapacidad() ?></p>
+                        <p class="card-text">Precio: $<?= number_format($producto->getPrecio(), 2) ?></p>
+                        <!-- Agregar botón "Agregar al carrito" con enlace al detalle del producto -->
+                        <a href="detalle_producto.php?id=<?= $producto->getId() ?>" class="btn btn-dark">Ver detalles</a>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+</div>
