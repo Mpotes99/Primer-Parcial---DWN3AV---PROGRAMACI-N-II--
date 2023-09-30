03dc72e4e6a1a3184c124c238bd6fdbeb51a7ff2
nicolasguaragna <fernando.guaragna@davinci.edu.ar>
2023-09-30 15:49:30 -0300

    ok

--- views/home.php
+++ views/home.php
@@ -2,7 +2,6 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12 p-0">
-                    <img src="img/bannerhome.png" alt="Productos Stanley" class="img-fluid cover-image">
                     <div class="cover-text">
                         <h1 class="text-center  py-5 text-black">Descubrí los productos<span class="stanley"><img src="img/stanley-logo.png" alt="logo2" class="logo1"></span></h1>
                         <a href="index.php?sec=vertodos" class="btn btn-dark btn-lg  custom-button">Ver más</a>
